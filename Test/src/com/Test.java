52ef1b7a0a90084dd63ae244bbf7a61a1674c8fb
TomAndLiu <1134593146@qq.com>
2019-11-13 11:55:41 +0800

    中文

--- Test/src/com/Test.java
+++ Test/src/com/Test.java
@@ -4,10 +4,10 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("你好");
+		System.out.println("浣犲ソ");
 		System.out.println("s");
-		System.out.println("大家好");
-		System.out.println("你好");
+		System.out.println("浣犲ソ");
+		System.out.println("浣犲ソ");
 		System.out.println("ada");
 		System.out.println("ada");
 		System.out.println("ada");
